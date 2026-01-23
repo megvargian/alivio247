e489930c9c59f46ab32ce171ccc928b1bac8e3af
Hrrrra <40232186+HasmigBaba@users.noreply.github.com>
2026-01-23 17:31:20 +0400

    index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>
-     Alivio24/7
+     Alivio 24/7
     </title>
     <meta name="description" content="Simple landind page" />
     <meta name="keywords" content="" />
