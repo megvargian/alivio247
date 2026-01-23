7a0bed5dc71868535c7785afa78dc432b9a6e907
Hrrrra <40232186+HasmigBaba@users.noreply.github.com>
2026-01-23 17:28:55 +0400

    index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>
-      Tailwind Starter Template - Landing Page Template: Tailwind Toolbox
+     Alivio24/7
     </title>
     <meta name="description" content="Simple landind page" />
     <meta name="keywords" content="" />
