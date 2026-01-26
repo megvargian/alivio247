d41ada7a990fe0bc9ce48e15f9a328cdc3fc83b8
Megvargian123 <mikekouyoumdjian@hotmail.com>
2026-01-26 20:01:11 +0200

    header

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include 'header.php';
                 <h1 class="my-4 text-5xl font-bold leading-tight">AI voice agents for</h1>
                 <p id="rotatingText" class="rotating-text"></p>
                 <button
-                    class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                    class="mx-auto lg:mx-0 hover:underline bg-[#0a3b88] text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
                     Start Now
                 </button>
             </div>
