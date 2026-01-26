00725fa02968a07d49128c435c296a42ad35691b
Megvargian123 <mikekouyoumdjian@hotmail.com>
2026-01-26 19:32:57 +0200

    header

--- header.php
+++ header.php
@@ -98,13 +98,42 @@ $current_page = basename($_SERVER['PHP_SELF']);
     </nav>
 
     <script>
-    // Toggle dropdown on mobile
-    function toggleMobileDropdown(event) {
-        if (window.innerWidth < 1024) {
-            event.preventDefault();
-            const dropdown = event.target.closest('.dropdown');
-            dropdown.classList.toggle('mobile-open');
+        // Toggle dropdown on mobile
+        function toggleMobileDropdown(event) {
+            if (window.innerWidth < 1024) {
+                event.preventDefault();
+                const dropdown = event.target.closest('.dropdown');
+                dropdown.classList.toggle('mobile-open');
+            }
         }
-    }
+        var navMenuDiv = document.getElementById("nav-content");
+        var navMenu = document.getElementById("nav-toggle");
+
+        document.onclick = check;
+
+        function check(e) {
+            var target = (e && e.target) || (event && event.srcElement);
+            if (!checkParent(target, navMenuDiv)) {
+                if (checkParent(target, navMenu)) {
+                    if (navMenuDiv.classList.contains("hidden")) {
+                        navMenuDiv.classList.remove("hidden");
+                    } else {
+                        navMenuDiv.classList.add("hidden");
+                    }
+                } else {
+                    navMenuDiv.classList.add("hidden");
+                }
+            }
+        }
+
+        function checkParent(t, elm) {
+            while (t.parentNode) {
+                if (t == elm) {
+                    return true;
+                }
+                t = t.parentNode;
+            }
+            return false;
+        }
     </script>
 <?php
